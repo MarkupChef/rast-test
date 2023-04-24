0ff7267adb2109eb2632932f099edbd0d3272e17
markupMashine <rastehaiev.r@gmail.com>
2023-04-24 18:09:30 +0300

    RT-10. Implemented redux-persis

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,26 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Result from './pages/Result/Result';
 import Test from './pages/Test';
-import store from './store/index';
+import store, { persistor } from './store/index';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path={'/'} element={<Home />} />
-            <Route path={'/test'} element={<Test />} />
-            <Route path={'/result'} element={<Result />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path={'/'} element={<Home />} />
+              <Route path={'/test'} element={<Test />} />
+              <Route path={'/result'} element={<Result />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
